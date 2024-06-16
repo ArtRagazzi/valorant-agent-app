54bed57b7d9ecf9c82c9cbfada7c6ba8472d6782
Artur <ao.ragazzi@gmail.com>
2024-06-16 12:16:46 -0300

    Finalizada Animções

--- app/src/main/java/com/artragazzi/valorantapiapp/ui/MainActivity.kt
+++ app/src/main/java/com/artragazzi/valorantapiapp/ui/MainActivity.kt
@@ -2,10 +2,12 @@ package com.artragazzi.valorantapiapp.ui
 
 import android.os.Bundle
 import android.util.Log
+import android.view.animation.AnimationUtils
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.isVisible
 import com.artragazzi.valorantapiapp.R
 import com.artragazzi.valorantapiapp.databinding.ActivityMainBinding
 import com.artragazzi.valorantapiapp.model.Agente
@@ -29,6 +31,20 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        binding.fabBuscar.setOnClickListener {
+            //Adicionando animação ao clicar em buscar (Aparecer apenas dps de carregado a chamada API)
+            val slideIn = AnimationUtils.loadAnimation(this, R.anim.slide_in)
+            binding.layoutInfo.startAnimation(slideIn)
+            binding.layoutInfo.isVisible = true
+        }
+
+
+        binding.btnRecuar.setOnClickListener {
+            val slideOut = AnimationUtils.loadAnimation(this, R.anim.slide_out)
+            binding.layoutInfo.startAnimation(slideOut)
+            binding.layoutInfo.isVisible = false
         }
 
 
